5ba79702a95b06f2fcfe5e3631d109ed2d618e4e
asernae7 <37089789+asernae7@users.noreply.github.com>
2018-06-07 18:19:52 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,99 +1,73 @@
 var express = require('express'),
  http = require('http'),
  path = require('path'),
- async = require('async'),
- gpio = require('rpi-gpio'),
  Gpio = require('onoff').Gpio,
  app = express();
- 
+
 //we use the port 3000
 app.set('port', 3000);
- 
-//we serve the static files of the directory /static
+
+
 app.use(express.static(path.join(__dirname, '/static')));
- 
-//we create the server
+
+//se crea el servidor
 var http = http.createServer(app).listen(app.get('port'), function(){
- console.log('Server was started by using the port ' + app.get('port'));
+ console.log('servidor corriendo en el puerto ' + app.get('port'));
 });
- 
-//we initialise socket.io
+
+//Inicializar el socket.io
 var io = require('socket.io')(http);
- 
-//the code controlling the tank starts here
-//----------------------------------
- 
-//we create the object tank
+
+
+//Tanque
 var tank = {
- 
- //we create an object with the used pins
- motors: {
-   leftFront: 4,
-   leftBack: 17,
-   rightFront: 18,
-   rightBack: 23
- },
- 
- //we open the gpio pins and set them as outputs
+
+ //Pines a ser usados
 init:function(){
-   gpio.setup(4,gpio.DIR_OUT,this);
-   gpio.setup(17,gpio.DIR_OUT,this);
-   gpio.setup(18,gpio.DIR_OUT,this);
-   gpio.setup(23,gpio.DIR_OUT,this);
    leftFront = new Gpio(4,'out');
    leftBack = new Gpio(17,'out');
    rightFront = new Gpio(18,'out');
    rightBack = new Gpio(23,'out');
 },
- 
- //in order to move the tank forward, we supply both motors
+
+ //Movimiento hacia adelante
  moveForward: function(){
      console.log('forward');
      leftFront.writeSync(1),
      rightFront.writeSync(1)
-     //gpio.write(this.motors.leftFront,true),
-     //gpio.write(this.motors.rightFront,true)
  },
- 
- //in order to move the tank backwards we supply the motors, but with inverse polarity
+
+ //Movimiento hacia atras
  moveBackward: function(){
      console.log('back');
      leftBack.writeSync(1),
      rightBack.writeSync(1)
-     //gpio.write(17,true),
-     //gpio.write(23,true)
  },
- 
- //in order to turn right, we supply the motor on the left
+
+ //Movimiento hacia la izquierda
  moveLeft: function(){
    console.log('left');
    leftFront.writeSync(1);
-   //gpio.write(4, true);
  },
- 
- //in order to turn left, we supply the motor on the right
+
+ //movimiento hacia la derecha
  moveRight: function(){
   console.log('right');
   rightFront.writeSync(1);
-  //gpio.write(23, true);
  },
- 
- //in order to stop both motors, we set the all pins to 0 value
+
+ //parar los motores
  stop: function(){
      leftFront.writeSync(0),
      leftBack.writeSync(0),
      rightFront.writeSync(0),
      rightBack.writeSync(0)
-     //gpio.write(4, false),
-     //gpio.write(17, false),
-     //gpio.write(18, false),
-     //gpio.write(23, false)
  }
 };
- 
-//we listen to new connections
+
+//esperar por la conección
 io.sockets.on('connection', function(socket) {
- //we listen the movement signal
+ //esperar la señal de los motores
  socket.on('move', function(direction) {
    switch(direction){
     case 'up':
@@ -110,13 +84,12 @@ io.sockets.on('connection', function(socket) {
        break;
    }
  });
- //we listen to the stop signal
+ //esperar la señal de parado
  socket.on('stop', function(dir){
    tank.stop();
    console.log('stop');
  });
 });
- 
-//we initialise the tank
-tank.init();
 
+//iniciar el objeto tank
+tank.init();
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
- var socket = io.connect(), //we connect by using a websocket
+ var socket = io.connect(), //uso de socket
    ui = {
      up: $('.btn-up'),
      left: $('.btn-left'),
@@ -9,50 +9,48 @@ $(function () {
    },
    activeClass = 'is-active',
    isPressed = false;
- 
- //we listen to key pressing
+
+ //esperar por el teclado
  $(document).keydown(function(e){
-   //ignores other keys pressed if a key is already pressed
-   //we do this in order to avoid sending out several commands
    //when we keep the key pressed
    if(isPressed) return;
- 
+
    isPressed = true;
    switch(e.which){
-     case 87: //code for the key w
+     case 87: //ASCII de w
        socket.emit('move', 'up');
        ui.up.addClass(activeClass);
        break;
-     case 65: //code for the key a
+     case 65:
        socket.emit('move', 'left');
        ui.left.addClass(activeClass);
        break;
-     case 83: //code for the key s
+     case 83:
        socket.emit('move', 'down');
        ui.down.addClass(activeClass);
        break;
-     case 68: //code for the key d
+     case 68:
        socket.emit('move', 'right');
        ui.right.addClass(activeClass);
        break;
    }
  });
- 
- //stops the motors when a key is released
+
+ //Al no oprimir una tecla envia el stop
  $(document).keyup(function(e){
    ui.all.removeClass(activeClass);
    socket.emit('stop');
    isPressed = false;
  });
- 
 
-/* Only touch */
- 
+
+/* Para dispositivos moviles */
+
  $( "#ButtonW" ).bind('touchstart', function() {
   socket.emit('move', 'up');
      ui.up.addClass(activeClass);
 });
- 
+
  $( "#ButtonW" ).bind('touchend', function() {
   ui.all.removeClass(activeClass);
    socket.emit('stop');
@@ -62,7 +60,7 @@ $(function () {
   socket.emit('move', 'left');
      ui.left.addClass(activeClass);
 });
- 
+
  $( "#ButtonA" ).bind('touchend', function() {
   ui.all.removeClass(activeClass);
    socket.emit('stop');
@@ -72,7 +70,7 @@ $(function () {
   socket.emit('move', 'down');
      ui.down.addClass(activeClass);
 });
- 
+
  $( "#ButtonS" ).bind('touchend', function() {
   ui.all.removeClass(activeClass);
    socket.emit('stop');
@@ -82,7 +80,7 @@ $(function () {
   socket.emit('move', 'right');
      ui.right.addClass(activeClass);
 });
- 
+
  $( "#ButtonD" ).bind('touchend', function() {
   ui.all.removeClass(activeClass);
    socket.emit('stop');
@@ -90,4 +88,3 @@ $(function () {
 /* Only touch */
 
 });
-
